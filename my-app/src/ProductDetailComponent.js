6e4536717eda656153ed80cd00c1606489628ee0
reymesson1 <reymesson1>
2021-05-24 19:07:59 -0400

    done
    70 30
    id dinamico

--- my-app/src/ProductDetailComponent.js
+++ my-app/src/ProductDetailComponent.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-// const src = 'https://images.unsplash.com/photo-1444065381814-865dc9da92c0'
-// const src = "http://localhost:8085/executed/"+ "7" + ".jpg"
-
 class ProductDetailComponent extends Component {
 
     constructor(props) {
@@ -11,8 +8,6 @@ class ProductDetailComponent extends Component {
         this.state = {
             id: 0,
             backgroundImage: `url(${'"http://localhost:8085/executed/"+ '+ this.props.match.params.id +' + ".jpg"'})`,
-            // backgroundImage: `url(${this.state.src})`,
-            // backgroundImage: '',
             backgroundPosition: '0% 0%',
             src: "http://localhost:8085/executed/"+ this.props.match.params.id + ".jpg"
         }
@@ -22,10 +17,6 @@ class ProductDetailComponent extends Component {
         this.setState({
             id: this.props.match.params.id,
             backgroundImage: `url(${"http://localhost:8085/executed/"+ this.props.match.params.id + ".jpg"})`,
-            // src: "http://localhost:8085/executed/"+ this.props.match.params.id + ".jpg"
-
-            // backgroundImage: "http://localhost:8085/executed/"+ this.state.id + ".png"
-            // backgroundImage: "http://localhost:8085/executed/"+ this.state.id + ".png"
         })
     }
 
@@ -46,9 +37,7 @@ class ProductDetailComponent extends Component {
                         <div className="row">
                             <div className="col-md-7">
                                 <figure onMouseMove={this.handleMouseMove} style={this.state}>
-                                    {/* <img src={"http://localhost:8085/executed/"+ this.state.id + ".png"}  alt="Avatar" style={{"width":"100%","height":"100%"}}/> */}
                                     <img src={this.state.src} alt="Avatar" style={{"width":"100%","height":"100%"}} />
-                                    {/* <img src={this.state.src} alt="Avatar" style={{"width":"100%","height":"100%"}} /> */}
                                 </figure>
                             </div>
                             <div className="col-md-5">
