cf15a412a1d2bd2381c66cdba8ad6074710c9fa3
reymesson1 <reymesson1>
2021-05-30 10:30:41 -0400

    done

--- my-app/public/sw.js
+++ my-app/public/sw.js
@@ -1,5 +1,22 @@
-self.addEventListener('install', function() {
-    console.log('Install!');
+const filesToCache = [
+    '/',
+    'index.html',
+    'offline.html',
+    '404.html',
+    'style.css',
+    'book.png'
+];
+
+const staticCacheName = 'our-second-cache';
+
+self.addEventListener('install', event => {
+    console.log('attempting to install serviec worker and cache static assets');
+    event.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 self.addEventListener('activate', function() {
