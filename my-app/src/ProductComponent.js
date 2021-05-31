7df4251de360d9f39b67b3a2d67d01796e475eab
reymesson1 <reymesson1>
2021-05-31 16:08:40 -0400

    done

--- my-app/src/ProductComponent.js
+++ my-app/src/ProductComponent.js
@@ -66,9 +66,9 @@ class Product extends Component {
                     <Link to={'/productdetail/'+index}> 
                         <div className="card" style={{'margin':'5%'}}>
                                 {/* <img src={"http://localhost:3000/book.png"}  alt="Avatar" style={{"width":"100%","height":"100%"}}/> */}
-                                <img src={"http://localhost:3000/"+ index + ".jpg"}  alt="Avatar" style={{"width":"100%","height":"100%"}}/>
+                                {/* <img src={"http://localhost:3000/"+ index + ".jpg"}  alt="Avatar" style={{"width":"100%","height":"100%"}}/> */}
                                 {/* <img src={"http://localhost:8085/executed/"+ index + ".jpg"}  alt="Avatar" style={{"width":"100%","height":"100%"}}/> */}
-                                {/* <img src={"http://143.198.124.234:8085/executed/"+ index + ".jpg"}  alt="Avatar" style={{"width":"100%","height":"100%"}}/> */}
+                                <img src={"http://143.198.124.234:8085/executed/"+ index + ".jpg"}  alt="Avatar" style={{"width":"100%","height":"100%"}}/>
                         </div>
                     </Link>
                 </div>
