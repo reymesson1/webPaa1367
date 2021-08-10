a360868a69cd7028c1bfa386efc92d0fe80fa608
Rey Messón <rmesson@skymaxdominicana.com>
2021-08-10 19:43:51 -0400

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,8 +21,8 @@ import CreateStyleComponent from './CreateStyleComponent';
 import CreateCompanyComponent from './CreateCompanyComponent';
 import  axios  from 'axios'
 
-// let API_URL = "http://localhost:8085";
-let API_URL = "http://143.198.171.44:8085";
+let API_URL = "http://localhost:8085";
+// let API_URL = "http://143.198.171.44:8085";
 
 const API_HEADERS = {
 
@@ -36,8 +36,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-        URLExternal: 'http://143.198.171.44:8085', 
-          // URLExternal: 'http://localhost:8085',
+        // URLExternal: 'http://143.198.171.44:8085', 
+          URLExternal: 'http://localhost:8085',
           showModal: false,
           newest: true,
           filterText: "",
