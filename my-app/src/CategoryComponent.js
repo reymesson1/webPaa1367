606646571f9d81ff09592c83b1051f3e0ae99ab5
Rey Messón <rmesson@skymaxdominicana.com>
2021-08-18 14:49:14 -0400

    done

--- my-app/src/CategoryComponent.js
+++ my-app/src/CategoryComponent.js
@@ -33,25 +33,25 @@ class CategoryComponent extends Component {
                 <div className="row">
                     <div className="col-md-3">
                         <Link to={'/home/bracelet'}> 
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>Bracelet</h1>
                         </Link>
                     </div>
                     <div className="col-md-3">
                         <Link to={'/home/rm'}> 
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>RM</h1>
                         </Link>
                     </div>
                     <div className="col-md-3">
                         <Link to={'/home/rings'}> 
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>Rings</h1>
                         </Link>
                     </div>
                     <div className="col-md-3">
                         <Link to={'/home/pendant'}> 
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>Pendant</h1>
                         </Link>
                     </div>
@@ -60,25 +60,25 @@ class CategoryComponent extends Component {
                 <div className="row">
                     <div className="col-md-3">
                         <Link to={'/home/pins'}> 
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>Pins</h1>
                         </Link>
                     </div>
                     <div className="col-md-3">
                     <Link to={'/home/necklace'}>                         
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>Necklace</h1>
                     </Link>
                     </div>
                     <div className="col-md-3">
                     <Link to={'/home/earings'}> 
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>Earings</h1>
                     </Link>
                     </div>
                     <div className="col-md-3">
                         <Link to={'/home/watches'}> 
-                            <img src={this.props.URLExternal+"/images/folder.jpg"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
+                            <img src={this.props.URLExternal+"/images/folder.JPG"} style={{"width":"50%","height":"80%","margin-left":"26%"}}  alt="Avatar"/>
                             <h1>Watches</h1>
                         </Link>
                     </div>
